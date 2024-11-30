29be1bbd1b10796e66f37ed7caa0d02f2d8e98bb
Laghav Mohan <lmohan@zlti.com>
2024-11-30 14:42:29 +0530

    Adding react build

--- src/main/java/org/chatapp/server/WebSocketServerApp.java
+++ src/main/java/org/chatapp/server/WebSocketServerApp.java
@@ -1,23 +1,30 @@
 package org.chatapp.server;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.tyrus.server.Server;
 
 
 
 public class WebSocketServerApp {
     public static void main(String[] args) {
-        // Specify the server details
-        Server server = new Server("localhost", 1235, "/chat", null, ChatServer.class);
+
 
         try {
-            // Start the server
+
+            HttpServer httpServer = HttpServer.createSimpleServer(null, 3000);
+            HttpHandler staticHandler = new StaticHttpHandler("./react-build");
+            httpServer.getServerConfiguration().addHttpHandler(staticHandler, "/");
+            httpServer.start();
+
+
+            Server server = new Server("localhost", 1235, "/chat", null, ChatServer.class);
+
             server.start();
             System.out.println("WebSocket server started...");
             System.in.read(); // Keep server running, press Enter to stop
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // Stop the server
-            server.stop();
         }
     }
 }
