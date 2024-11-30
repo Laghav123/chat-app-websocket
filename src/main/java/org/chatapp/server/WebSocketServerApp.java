8e15200b2660d86f1ed7e1bc3359fb2d1d33a92a
Laghav Mohan <lmohan@zlti.com>
2024-11-30 14:58:28 +0530

    making app production ready

--- src/main/java/org/chatapp/server/WebSocketServerApp.java
+++ src/main/java/org/chatapp/server/WebSocketServerApp.java
@@ -18,8 +18,8 @@ public class WebSocketServerApp {
             httpServer.start();
 
 
-            Server server = new Server("localhost", 1235, "/chat", null, ChatServer.class);
-
+            int port = Integer.parseInt(System.getenv("PORT")); // Fetch Heroku-assigned port
+            Server server = new Server("0.0.0.0", port, "/", null, ChatServer.class);
             server.start();
             System.out.println("WebSocket server started...");
             System.in.read(); // Keep server running, press Enter to stop
