3374f650f9612680648bf04f1760c7c54a314470
Laghav Mohan <lmohan@zlti.com>
2024-12-01 01:50:26 +0530

    Removing ws server for testing http server

--- src/main/java/org/chatapp/server/WebSocketServerApp.java
+++ src/main/java/org/chatapp/server/WebSocketServerApp.java
@@ -1,7 +1,9 @@
 package org.chatapp.server;
+import jakarta.websocket.server.ServerContainer;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import org.glassfish.tyrus.container.grizzly.server.GrizzlyServerContainer;
 import org.glassfish.tyrus.server.Server;
 
 
@@ -14,15 +16,48 @@ public class WebSocketServerApp {
             int port = Integer.parseInt(System.getenv("PORT")); // Fetch Heroku-assigned port
 
             HttpServer httpServer = HttpServer.createSimpleServer(null, port);
-            HttpHandler staticHandler = new StaticHttpHandler("./react-build");
-            httpServer.getServerConfiguration().addHttpHandler(staticHandler, "/");
+//            HttpHandler staticHandler = new StaticHttpHandler("./react-build");
+            httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler() {
+                @Override
+                public void service(org.glassfish.grizzly.http.server.Request request,
+                                    org.glassfish.grizzly.http.server.Response response) throws Exception {
+                    response.setContentType("text/plain");
+                    response.getWriter().write("This is a web socket server");
+                }
+            }, "/");
+            httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler() {
+                @Override
+                public void service(org.glassfish.grizzly.http.server.Request request,
+                                    org.glassfish.grizzly.http.server.Response response) throws Exception {
+                    response.setContentType("text/plain");
+                    response.getWriter().write("This is a web socket server");
+                }
+            }, "/favicon.ico");
+
+//            ServerContainer serverContainer = new
+
             httpServer.start();
+//            System.out.println("Enabled System.in.read()");
+//            System.in.read();
 
+            // Add a shutdown hook to gracefully stop the server
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down server...");
+                httpServer.shutdownNow();
+            }));
 
-            Server server = new Server("0.0.0.0", port, "/", null, ChatServer.class);
-            server.start();
-            System.out.println("WebSocket server started...");
-            System.in.read(); // Keep server running, press Enter to stop
+            // Block the main thread indefinitely
+            try {
+                Thread.currentThread().join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Restore the interrupt status
+                System.err.println("Main thread interrupted, exiting...");
+            }
+
+//            Server server = new Server("localhost", port, "/", null, ChatServer.class);
+//            server.start();
+//            System.out.println("WebSocket server started...");
+//            System.in.read(); // Keep server running, press Enter to stop
         } catch (Exception e) {
             e.printStackTrace();
         }
