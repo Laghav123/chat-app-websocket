40212df6b735c3817d8f5b403091912364476b99
Laghav Mohan <lmohan@zlti.com>
2024-11-30 21:51:45 +0530

    removed grizzly server container

--- src/main/java/org/chatapp/server/WebSocketServerApp.java
+++ src/main/java/org/chatapp/server/WebSocketServerApp.java
@@ -1,7 +1,4 @@
 package org.chatapp.server;
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.tyrus.server.Server;
 
 
@@ -12,12 +9,6 @@ public class WebSocketServerApp {
 
         try {
             int port = Integer.parseInt(System.getenv("PORT")); // Fetch Heroku-assigned port
-
-            HttpServer httpServer = HttpServer.createSimpleServer(null, port);
-            HttpHandler staticHandler = new StaticHttpHandler("./react-build");
-            httpServer.getServerConfiguration().addHttpHandler(staticHandler, "/");
-            httpServer.start();
-
 
             Server server = new Server("0.0.0.0", port, "/", null, ChatServer.class);
             server.start();
